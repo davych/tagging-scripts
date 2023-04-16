c4ccaf2ef63d2728005da1c8b6fcffbcbb8785c3
davy_chen <davy_chen@epam.com>
2023-04-16 17:58:17 +0800

    chore: checking workflow

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  tagFormat: 'v${version}',
+  "branches": [
+    {name: 'master'},
+    {name: 'beta', channel: 'beta', prerelease: 'beta'},
+  ],
+  "plugins": [
+    '@semantic-release/commit-analyzer', 
+    '@semantic-release/release-notes-generator', 
+    ["@semantic-release/npm", {
+      "npmPublish": true,
+      "tarballDir": "dist"
+    }],
+    '@semantic-release/github',
+    ['@semantic-release/git', {
+      "assets": [],
+      "message": "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}"
+    }]
+  ]
+}
