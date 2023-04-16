71b20a441d842f183d145d02b602c61ae248ac8a
davy_chen <davy_chen@epam.com>
2023-04-16 19:40:18 +0800

    feat: release 1 verison

--- release.config.js
+++ release.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   tagFormat: 'v${version}',
-  repositoryUrl: "git@github.com:davych/tagging-scripts.git",
   "branches": [
     {name: 'main'},
     {name: 'beta', channel: 'beta', prerelease: 'beta'},
@@ -8,13 +7,19 @@ module.exports = {
   "plugins": [
     '@semantic-release/commit-analyzer', 
     '@semantic-release/release-notes-generator', 
+    [
+      "@semantic-release/github",
+      {
+        "successComment": false,
+        "failTitle": false
+      }
+    ],
     ["@semantic-release/npm", {
       "npmPublish": true,
       "tarballDir": "dist"
     }],
-    '@semantic-release/github',
     ['@semantic-release/git', {
-      "assets": [],
+      "assets": ['package.json', 'package-lock.json', 'dist'],
       "message": "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}"
     }]
   ]
